966c7346a31cda0edee5258bed4a49ae7490aa7d
Thiago Spart <user000.000resu@gmail.com>
2021-02-01 16:14:32 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -43,3 +43,27 @@ const Transaction = {
 
 	}
 }
+
+const DOM = {
+	transactionsContainer: document.querySelector("#data-table tbody"),
+	addTransaction(transaction, index) {
+		const tr = document.createElement('tr')//,
+			//transaction = transaction[index]
+		tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+		DOM.transactionsContainer.appendChild(tr)
+	},
+
+	innerHTMLTransaction(transaction) {
+		const html = `
+			<td class="description">${transaction.description}</td>
+			<td class="expense">${transaction.amount}</td>
+			<td class="date">${transaction.date}</td>
+			<td><img src="assets/minus.svg" alt="Remover transação"></td>
+		` 
+		return html
+	}
+}
+
+transactions.forEach(function(transaction) {
+	DOM.addTransaction(transactions)
+})
