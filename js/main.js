3361ca1f248d1ca21e2e3e1089302e23741de5b8
Thiago Spart <user000.000resu@gmail.com>
2021-02-02 00:33:15 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -30,17 +30,36 @@ const transactions = [{
 		description: "Internet",
 		amount: -20000,
 		date: "23/01/2021"
-	}]
+}]
 
 const Transaction = {
+	all: transactions,
+	add(transaction) {
+		Transaction.all.push(transaction)
+		App.reload()
+	},
 	incomes() {
+		let income = 0 
+		Transaction.all.forEach(transaction => {
+			if (transaction.amount > 0) {
+				income += transaction.amount
+			} 
+		})
 
+		return income
 	},
 	expenses() {
+		let expense = 0 
+		Transaction.all.forEach(transaction => {
+			if (transaction.amount < 0) {
+				expense += transaction.amount
+			} 
+		})
 
+		return expense
 	},
 	total() {
-
+		return Transaction.incomes() + Transaction.expenses()
 	}
 }
 
@@ -52,7 +71,6 @@ const DOM = {
 		tr.innerHTML = DOM.innerHTMLTransaction(transaction)
 		DOM.transactionsContainer.appendChild(tr)
 	},
-
 	innerHTMLTransaction(transaction) {
 		const cssClass = transaction.amount > 0 ? "income" : "expense" 
 		const amount = Utils.formatCurrency(transactions.amount) 
@@ -63,6 +81,17 @@ const DOM = {
 			<td><img src="assets/minus.svg" alt="Remover transação"></td>
 		` 
 		return html
+	},
+	updateBalance () {
+		document
+			.getElementById("incomeDisplay")
+			.innerHTML = Utils.formatCurrency(Transaction.incomes()) 
+		document
+			.getElementById("expenseDisplay")
+			.innerHTML = Utils.formatCurrency(Transaction.expenses())
+		document
+			.getElementById("totalDisplay")
+			.innerHTML = Utils.formatCurrency(Transaction.total())
 	}
 }
 
@@ -79,6 +108,24 @@ const Utils = {
 	}
 }
 
-transactions.forEach(function(transaction) {
-	DOM.addTransaction(transactions)
+const App = {
+	init() {
+		Transaction.all.forEach(transaction => {
+			DOM.addTransaction(transactions)
+		})
+
+		DOM.updateBalance()
+	},
+	reload() {
+		App.init()
+	}
+}
+
+Transaction.add({
+	id: 39,
+	description: "descrição",
+	amount: 20000,
+	date: "23/01/2021"
 })
+
+App.reload()
